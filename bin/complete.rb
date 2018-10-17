0052184070598317df4e08d68117a3290868a87b
Ivan Kapelyukhin <ikapelyukhin@suse.com>
2018-10-17 15:07:53 +0200

    Make completion script faster (700 ms -> 180 ms)

--- bin/complete.rb
+++ bin/complete.rb
@@ -2,11 +2,9 @@
 
 # Thor / ActiveSupport path magic
 rmt_path = File.expand_path('..', __dir__)
-require_relative '../config/boot'
 $LOAD_PATH.unshift File.join(rmt_path, 'lib')
 
 require 'active_support'
-
 relative_load_paths = %w[lib lib/rmt/cli/].map { |dir| File.join(rmt_path, dir) }
 ActiveSupport::Dependencies.autoload_paths += relative_load_paths
 # magic ends here
